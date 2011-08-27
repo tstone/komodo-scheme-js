83a3129de1e06e4e94c16c75fe3737943e7855e5
T. Stone <wastingtape@gmail.com>
2011-08-26 23:27:44 -0700

    Got node testing working, changed object to make more sense relative to exports

--- src/komodo-scheme.js
+++
@@ -1,8 +0,0 @@
-
-
-/* ================================================================
-  KomodoScheme
-  An object for understanding Komodo theme files
-  =============================================================== */
-
-var KomodoScheme = function(){};
--- test/specs/ksf-color-spec.js
+++ test/specs/ksf-color-spec.js
@@ -1,14 +1,14 @@
+var Color = require('komodo-scheme').Color;
 
+describe('KomodoScheme.Color', function() {
 
-describe('KSFColor', function() {
-
-    it ('Accepts a BGR value and renders it into RGB and hex', function(){
+    it ('accepts a BGR value and renders it into RGB and hex', function(){
         // White
-        var color = new KSFColor('16777215');
+        var color = new Color('16777215');
         expect(color.hex).toEqual('ffffff');
         expect(color.rgb).toEqual([255,255,255]);
         // Some other random color
-        color = new KSFColor('7872391');
+        color = new Color('7872391');
         expect(color.hex).toEqual('781f87');
         expect(color.rgb).toEqual([120,31,135]);
     });
